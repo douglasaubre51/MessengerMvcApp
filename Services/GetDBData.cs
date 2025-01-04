a49ce4f0ee4e82cc76121ea754f1450d44adc256
Allen Bose <boseallen192@gmail.com>
2025-01-04 14:08:54 +0530

    created Selectdata interface

---
+++ Services/GetDBData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MessengerMvcApp.Services
+{
+    public interface GetDBData
+    {
+        public DataSet SelectData(string query, IConfiguration configuration)
+        {
+            string? connectionString = configuration.GetConnectionString("MessengerDataString");
+
+            DataSet dataSet = new DataSet();
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection))
+                {
+                    sqlDataAdapter.Fill(dataSet, "UserDetails");
+                }
+            }
+
+            return dataSet;
+        }
+    }
+}
