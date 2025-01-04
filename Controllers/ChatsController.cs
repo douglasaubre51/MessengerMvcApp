e7c6b0c991e5ee80702d9239607b77cdc0eda7d2
Allen Bose <boseallen192@gmail.com>
2025-01-04 11:08:05 +0530

    testing git creating a chat controller and view

---
+++ Controllers/ChatsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerMvcApp.Controllers
+{
+    public class ChatsController : Controller
+    {
+        public IActionResult ChatsView()
+        {
+            return View();
+        }
+    }
+}
