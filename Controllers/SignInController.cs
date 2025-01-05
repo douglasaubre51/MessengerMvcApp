f5a3d86312d4675f344aa09a71fe38131bb4f242
Allen Bose <boseallen192@gmail.com>
2025-01-05 20:46:14 +0530

    added database connection in signin controller

--- Controllers/SignInController.cs
+++ Controllers/SignInController.cs
@@ -1,10 +1,19 @@
 ﻿using MessengerMvcApp.Models;
+using MessengerMvcApp.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace MessengerMvcApp.Controllers
 {
     public class SignInController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public SignInController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult SignInView()
         {
             return View();
@@ -15,6 +24,20 @@ namespace MessengerMvcApp.Controllers
         {
             if (ModelState.IsValid)
             {
+                GetDBData getDBData = new GetDBData();
+                DataTable dataTable = new DataTable();
+
+                string query = "select EmailID,Password from UserDetails where EmailID=@emailId";
+                
+                dataTable = getDBData.SelectData(query, _configuration);
+
+                if (dataTable.Rows.Count == 0)
+                {
+                    return View("SignInView", model);
+                }
+
+
+
                 return RedirectToAction("ChatsView", "Chats");
             }
             else
